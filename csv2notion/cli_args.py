a051eb48ce2140c15131aab232bdc32a70b8b6c3
vzhd1701 <vzhd1701@gmail.com>
2022-05-23 20:54:08 +0500

    fix: split --help options into groups

--- csv2notion/cli_args.py
+++ csv2notion/cli_args.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-from typing import Any, Dict, List, Sequence
+from typing import Any, Dict, List, Sequence, Tuple, Union
 
 from csv2notion.notion_convert_map import map_icon
 from csv2notion.utils_exceptions import CriticalError
@@ -11,216 +11,251 @@ from csv2notion.version import __version__
 
 def parse_args(argv: Sequence[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        prog="csv2notion", description="Import/Merge CSV file into Notion database"
+        prog="csv2notion",
+        description="Import/Merge CSV file into Notion database",
+        usage="%(prog)s [-h] --token TOKEN [--url URL] [OPTION]... FILE",
+        add_help=False,
+        formatter_class=lambda prog: argparse.RawTextHelpFormatter(
+            prog, max_help_position=50
+        ),
     )
 
-    schema: Dict[str, Dict[str, Any]] = {
-        "csv_file": {
-            "type": Path,
-            "help": "CSV file to upload",
-            "metavar": "FILE",
+    schema: Dict[str, Dict[Union[str, Tuple[str, str]], Any]] = {
+        "POSITIONAL": {
+            "csv_file": {
+                "type": Path,
+                "help": "CSV file to upload",
+                "metavar": "FILE",
+            }
         },
-        "--token": {
-            "help": (
-                "Notion token, stored in token_v2 cookie for notion.so"
-                " [NEEDED FOR UPLOAD]"
-            ),
-            "required": True,
+        "general options": {
+            "--token": {
+                "help": "Notion token, stored in token_v2 cookie for notion.so",
+                "required": True,
+            },
+            "--url": {
+                "help": (
+                    "Notion database URL; if none is provided,"
+                    " will create a new database"
+                ),
+                "metavar": "URL",
+            },
+            "--max-threads": {
+                "type": int,
+                "default": 5,
+                "help": "upload threads (default: 5)",
+                "metavar": "NUMBER",
+            },
+            "--log": {
+                "type": Path,
+                "metavar": "FILE",
+                "help": "file to store program log",
+            },
+            "--verbose": {
+                "action": "store_true",
+                "default": False,
+                "help": "output debug information",
+            },
+            "--version": {
+                "action": "version",
+                "version": f"%(prog)s {__version__}",
+            },
+            ("-h", "--help"): {
+                "action": "help",
+                "help": "show this help message and exit",
+            },
         },
-        "--url": {
-            "help": (
-                "Notion database URL; if none is provided,"
-                " will create a new database"
-            ),
-            "metavar": "URL",
+        "columns options": {
+            "--custom-types": {
+                "help": (
+                    "comma-separated list of custom types to use for non-key columns;"
+                    "\nif none is provided, types will be guessed from CSV values"
+                    "\n(can also used with --add-missing-columns flag)"
+                ),
+                "metavar": "TYPES",
+            },
+            "--add-missing-columns": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "if columns are present in CSV but not in Notion DB,"
+                    " add them to Notion DB"
+                ),
+            },
         },
-        "--max-threads": {
-            "type": int,
-            "default": 5,
-            "help": "upload threads (default: 5)",
-            "metavar": "NUMBER",
+        "merging options": {
+            "--merge": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "merge CSV with existing Notion DB rows,"
+                    " first column will be used as a key"
+                ),
+            },
+            "--merge-only-column": {
+                "action": "append",
+                "help": (
+                    "CSV column that should be updated on merge;"
+                    "\nwhen provided, other columns will be ignored"
+                    "\n(use multiple times for multiple columns)"
+                ),
+                "metavar": "COLUMN",
+            },
+            "--merge-skip-new": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "skip new rows in CSV that are not already in Notion DB"
+                    " during merge"
+                ),
+            },
         },
-        "--custom-types": {
-            "help": (
-                "comma-separated list of custom types to use for non-key columns;"
-                " if none is provided, types will be guessed from CSV values"
-                " (used when creating a new database or"
-                " --missing-columns-action is set to 'add')"
-            ),
-            "metavar": "TYPES",
+        "relations options": {
+            "--add-missing-relations": {
+                "action": "store_true",
+                "default": False,
+                "help": "add missing entries into linked Notion DB",
+            },
         },
-        "--image-column": {
-            "help": (
-                "CSV column that points to URL or image file"
-                " that will be embedded for that row"
-            ),
-            "metavar": "COLUMN",
+        "page cover options": {
+            "--image-column": {
+                "help": (
+                    "CSV column that points to URL or image file"
+                    " that will be embedded for that row"
+                ),
+                "metavar": "COLUMN",
+            },
+            "--image-column-keep": {
+                "action": "store_true",
+                "default": False,
+                "help": "keep image CSV column as a Notion DB column",
+            },
+            "--image-column-mode": {
+                "choices": ["cover", "block"],
+                "default": "block",
+                "help": (
+                    "upload image as [cover] or insert it as [block]"
+                    " (default: block)"
+                ),
+            },
+            "--image-caption-column": {
+                "help": (
+                    "CSV column that points to text caption"
+                    " that will be added to the image block"
+                    "\nif --image-column-mode is set to 'block'"
+                ),
+                "metavar": "COLUMN",
+            },
+            "--image-caption-column-keep": {
+                "action": "store_true",
+                "default": False,
+                "help": "keep image caption CSV column as a Notion DB column",
+            },
         },
-        "--image-column-keep": {
-            "action": "store_true",
-            "default": False,
-            "help": "keep image CSV column as a Notion DB column",
+        "page icon options": {
+            "--icon-column": {
+                "help": (
+                    "CSV column that points to emoji, URL or image file"
+                    "\nthat will be used as page icon for that row"
+                ),
+                "metavar": "COLUMN",
+            },
+            "--icon-column-keep": {
+                "action": "store_true",
+                "default": False,
+                "help": "keep icon CSV column as a Notion DB column",
+            },
+            "--default-icon": {
+                "help": (
+                    "Emoji, URL or image file"
+                    " that will be used as page icon for every row by default"
+                ),
+                "metavar": "ICON",
+            },
         },
-        "--image-column-mode": {
-            "choices": ["cover", "block"],
-            "default": "block",
-            "help": (
-                "upload image as [cover] or insert it as [block]" " (default: block)"
-            ),
-        },
-        "--image-caption-column": {
-            "help": (
-                "CSV column that points to text caption"
-                " that will be added to the image block"
-                " if --image-column-mode is set to 'block'"
-            ),
-            "metavar": "COLUMN",
-        },
-        "--image-caption-column-keep": {
-            "action": "store_true",
-            "default": False,
-            "help": "keep image caption CSV column as a Notion DB column",
-        },
-        "--icon-column": {
-            "help": (
-                "CSV column that points to emoji, URL or image file"
-                " that will be used as page icon for that row"
-            ),
-            "metavar": "COLUMN",
-        },
-        "--icon-column-keep": {
-            "action": "store_true",
-            "default": False,
-            "help": "keep icon CSV column as a Notion DB column",
-        },
-        "--default-icon": {
-            "help": (
-                "Emoji, URL or image file"
-                " that will be used as page icon for every row by default"
-            ),
-            "metavar": "ICON",
-        },
-        "--add-missing-columns": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "if columns are present in CSV but not in Notion DB,"
-                " add them to Notion DB"
-            ),
-        },
-        "--add-missing-relations": {
-            "action": "store_true",
-            "default": False,
-            "help": "add missing entries into linked Notion DB",
-        },
-        "--fail-on-relation-duplicates": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "fail if any linked DBs in relation columns have duplicate entries;"
-                " otherwise, first entry in alphabetical order"
-                " will be treated as unique when looking up relations"
-            ),
-        },
-        "--fail-on-duplicates": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "fail if Notion DB or CSV has duplicates in key column,"
-                " useful when sanitizing before merge to avoid ambiguous mapping"
-            ),
-        },
-        "--fail-on-duplicate-csv-columns": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "fail if CSV has duplicate columns; otherwise last column will be used"
-            ),
-        },
-        "--fail-on-conversion-error": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "fail if any column type conversion error occurs;"
-                " otherwise errors will be replaced with empty strings"
-            ),
-        },
-        "--fail-on-inaccessible-relations": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "fail if any relation column points to a Notion DB that"
-                " is not accessible to the current user;"
-                " otherwise those columns will be ignored"
-            ),
-        },
-        "--fail-on-missing-columns": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "fail if columns are present in CSV but not in Notion DB;"
-                " otherwise those columns will be ignored"
-            ),
-        },
-        "--fail-on-unsupported-columns": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "fail if DB has columns that are not supported by this tool;"
-                " otherwise those columns will be ignored"
-                " (columns with type created_by, last_edited_by, rollup or formula)"
-            ),
-        },
-        "--merge": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "merge CSV with existing Notion DB rows,"
-                " first column will be used as a key"
-            ),
-        },
-        "--merge-only-column": {
-            "action": "append",
-            "help": (
-                "CSV column that should be updated on merge;"
-                " when provided, other columns will be ignored"
-                " (use multiple times for multiple columns)"
-            ),
-            "metavar": "COLUMN",
-        },
-        "--merge-skip-new": {
-            "action": "store_true",
-            "default": False,
-            "help": (
-                "skip new rows in CSV that are not already in Notion DB during merge"
-            ),
-        },
-        "--mandatory-column": {
-            "action": "append",
-            "help": (
-                "CSV column that cannot be empty"
-                " (use multiple times for multiple columns)"
-            ),
-            "metavar": "COLUMN",
-        },
-        "--log": {
-            "type": Path,
-            "metavar": "FILE",
-            "help": "file to store program log",
-        },
-        "--verbose": {
-            "action": "store_true",
-            "default": False,
-            "help": "output debug information",
-        },
-        "--version": {
-            "action": "version",
-            "version": f"%(prog)s {__version__}",
+        "validation options": {
+            "--mandatory-column": {
+                "action": "append",
+                "help": (
+                    "CSV column that cannot be empty"
+                    " (use multiple times for multiple columns)"
+                ),
+                "metavar": "COLUMN",
+            },
+            "--fail-on-relation-duplicates": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "fail if any linked DBs in relation columns have duplicate entries;"
+                    "\notherwise, first entry in alphabetical order"
+                    "\nwill be treated as unique when looking up relations"
+                ),
+            },
+            "--fail-on-duplicates": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "fail if Notion DB or CSV has duplicates in key column,"
+                    "\nuseful when sanitizing before merge to avoid ambiguous mapping"
+                ),
+            },
+            "--fail-on-duplicate-csv-columns": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "fail if CSV has duplicate columns;"
+                    "\notherwise last column will be used"
+                ),
+            },
+            "--fail-on-conversion-error": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "fail if any column type conversion error occurs;"
+                    "\notherwise errors will be replaced with empty strings"
+                ),
+            },
+            "--fail-on-inaccessible-relations": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "fail if any relation column points to a Notion DB that"
+                    "\nis not accessible to the current user;"
+                    "\notherwise those columns will be ignored"
+                ),
+            },
+            "--fail-on-missing-columns": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "fail if columns are present in CSV but not in Notion DB;"
+                    "\notherwise those columns will be ignored"
+                ),
+            },
+            "--fail-on-unsupported-columns": {
+                "action": "store_true",
+                "default": False,
+                "help": (
+                    "fail if DB has columns that are not supported by this tool;"
+                    "\notherwise those columns will be ignored"
+                    "\n(columns with type created_by, last_edited_by,"
+                    " rollup or formula)"
+                ),
+            },
         },
     }
 
-    for arg, arg_params in schema.items():
-        parser.add_argument(arg, **arg_params)
+    for group_name, group_args in schema.items():
+        group: argparse._ActionsContainer
+
+        if group_name == "POSITIONAL":
+            group = parser
+        else:
+            group = parser.add_argument_group(group_name)
+
+        for arg, arg_params in group_args.items():
+            opt_arg = [arg] if isinstance(arg, str) else arg
+            group.add_argument(*opt_arg, **arg_params)
 
     parsed_args = parser.parse_args(argv)
 
